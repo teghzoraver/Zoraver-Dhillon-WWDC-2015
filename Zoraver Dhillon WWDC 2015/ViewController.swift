9ec48c803c0f59a0afb4445dd9d61a73703ccfa6
teghzoraver <teghzoraver@gmail.com>
2015-04-25 20:23:57 -0700

    My WWDC 2015 Student Application

---
+++ Zoraver Dhillon WWDC 2015/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  Zoraver Dhillon WWDC 2015
+//
+//  Created by Zoraver Singh Dhillon on 4/25/15.
+//  Copyright (c) 2015 Zoraver Singh Dhillon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, SphereMenuDelegate {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        self.view.backgroundColor = UIColor(red:0.2, green:0.38, blue:0.8, alpha:1)
+        let start = UIImage(named: "start")
+        let image1 = UIImage(named: "swift")
+        let image2 = UIImage(named: "dhol")
+        let image3 = UIImage(named: "cards")
+        let image4 = UIImage(named: "rubiks")
+        var images:[UIImage] = [image1!,image2!,image3!,image4!]
+        var menu = SphereMenu(startPoint: CGPointMake(160, 320), startImage: start!, submenuImages:images)
+        menu.delegate = self
+        self.view.addSubview(menu)
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func sphereDidSelected(index: Int) {
+        println("\(index)")
+    }
+    
+}
